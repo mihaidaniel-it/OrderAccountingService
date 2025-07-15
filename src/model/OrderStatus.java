140083a8ef880044fe4f25c0148dde825cdb8cd7
Daniel <mihaidaniel.it@gmail.com>
2025-07-15 14:05:13 +0300

    Added const fields for Order Status and method isFinal()

--- src/model/OrderStatus.java
+++ src/model/OrderStatus.java
@@ -1,4 +1,19 @@
 package model;
 
 public enum OrderStatus {
+//    Fields
+    NEW,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED;
+
+//    Methods
+    public static boolean isFinal(Order order) {
+        if (order.status() == SHIPPED || order.status() == DELIVERED) {
+            return true;
+        }
+
+        return false;
+    }
 }
