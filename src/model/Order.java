3226ddfd066c072b1cfadbbad7a3df8431ab3783
Daniel <mihaidaniel.it@gmail.com>
2025-07-15 13:59:17 +0300

    Added method which show order total price

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,4 +1,14 @@
 package model;
 
-public record Order() {
+public record Order(
+        String productName,
+        Double pricePerUnit,
+        Integer quantity,
+        OrderStatus status) {
+//    Methods
+    public double orderTotalPrice() {
+        Double totalPrice = this.pricePerUnit * this.quantity;
+        double primitiveTotalPrice = totalPrice;
+        return primitiveTotalPrice;
+    }
 }
